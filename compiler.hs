13eef38e5531523a389cfa272ff7a72fa5f7cf1a
tomas <tomasarmento@gmail.com>
2023-12-16 15:19:20 +0000

    added loop to the code

--- compiler.hs
+++ compiler.hs
@@ -10,7 +10,7 @@ data Inst =
     Branch Code Code | Loop Code Code
     deriving Show
 type Code = [Inst]
-data Storeddata= TT | FF | N Integer 
+data Storeddata= TT | FF | N Integer
     deriving (Eq, Show)
 -- a configuração da máquina
 -- a pilha é uma lista de valores
@@ -28,25 +28,43 @@ compile (OpAdd e1 e2)
 compile (OpMult e1 e2)
     = compile e1 ++ compile e2 ++ [Mult]
 
-findStored:: [Stored]->String->([Stored],Storeddata)
+findStored:: [Stored]->String->Storeddata
 findStored [] _ = error "Value not found"
 findStored ((x,stored):l) s
-    | x == s =(l,stored)
-    | otherwise = 
-        let (list , result) = findStored l s in
-            ((x,stored):list,result)
+    | x == s = stored
+    | otherwise = findStored l s
+
+addStored:: [Stored]->(String,Storeddata)->[Stored]
+addStored [] new = [new]
+addStored ((x,stored):l) (new_s,new_stored)
+    | x == new_s = (new_s,new_stored):l
+    | otherwise = (x,stored):addStored l (new_s,new_stored)
+
 exec :: State -> State
 exec (stack, [],stored) = (stack, [],stored)
-
 exec (stack, Push v:code,stored)=exec (push (N v) stack, code,stored)
 exec (stack, Tru:code,stored)= (push TT stack, code,stored)
 exec (stack, Fals:code,stored)= (push FF stack, code,stored)
+
 exec (stack, Neg:code,stored)
     | top stack == TT = (push FF (pop stack), code,stored)
     | top stack == FF = (push TT (pop stack), code,stored)
     | otherwise = error "not bollean on NEG"
 
-exec (stack, State s:code,stored)= (pop stack, code,(s,top stack):stored)
+exec (stack, State s:code,stored)= (pop stack, code,addStored stored (s,top stack))
+
+exec (stack, Branch c1 c2:code,stored)
+    | top stack == TT = exec (pop stack, c1 ++ code,stored)
+    | top stack == FF = exec (pop stack, c2 ++ code,stored)
+    | otherwise = error "not bollean on Branch"
+exec (stack, Noop:code,stored)=
+    exec (stack, code,stored)
+
+exec (stack, Loop c1 c2:code,stored)=
+    let loopcode = c2++[Loop c1 c2] in
+        let code1 = (Branch  loopcode [Noop]):code  in
+            let code2 = c1 ++ code1 in
+                (stack, code2 ,stored)
 
 exec (stack, Equ:code,stored)=
     let v1 = top stack in
@@ -59,11 +77,11 @@ exec (stack, Le:code,stored)=
         let N v2 = top (pop stack ) in
             if v1 <= v2 then (push TT (pop (pop stack)), code,stored)
             else (push FF (pop (pop stack)), code,stored)
-                       
 
-exec (stack, Fetch s:code,stored)= 
-        let (newstoredvalues,storedvalue) = findStored stored s in
-            (push storedvalue stack, code,newstoredvalues)
+
+exec (stack, Fetch s:code,stored)=
+        let storedvalue = findStored stored s in
+            (push storedvalue stack, code,stored)
 exec (stack, Add:code,stored)=
     let N v1 = top stack in
         let N v2 = top (pop stack ) in
@@ -87,7 +105,7 @@ storeddataToString FF = "False"
 storeddataToString (N n) = show n
 
 stack2Str :: Stack Storeddata -> String
-stack2Str stack 
+stack2Str stack
     | isEmpty stack = ""
     | isEmpty (pop stack) = storeddataToString (top stack)
     | otherwise = storeddataToString (top stack) ++ "," ++ stack2Str (pop stack)
@@ -114,4 +132,4 @@ run c =
 
 testAssembler :: Code -> (String, String)
 testAssembler code = (stack2Str stack, state2Str state)
-    where (stack,_,state) = runState(createEmptyStack,code,createEmptyStored)
+    where (stack,_,state) = runState (createEmptyStack,code,createEmptyStored)
