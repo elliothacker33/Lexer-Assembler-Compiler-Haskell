806ad70104e7f5d93b5db418f9458d72f8d97398
tomas <tomasarmento@gmail.com>
2023-12-16 14:23:47 +0000

    implemented comparations

--- compiler.hs
+++ compiler.hs
@@ -28,23 +28,42 @@ compile (OpAdd e1 e2)
 compile (OpMult e1 e2)
     = compile e1 ++ compile e2 ++ [Mult]
 
-findStored:: [Stored]->String->Storeddata
+findStored:: [Stored]->String->([Stored],Storeddata)
 findStored [] _ = error "Value not found"
 findStored ((x,stored):l) s
-    | x == s =stored
-    | otherwise = findStored l s
+    | x == s =(l,stored)
+    | otherwise = 
+        let (list , result) = findStored l s in
+            ((x,stored):list,result)
 exec :: State -> State
 exec (stack, [],stored) = (stack, [],stored)
 
 exec (stack, Push v:code,stored)=exec (push (N v) stack, code,stored)
 exec (stack, Tru:code,stored)= (push TT stack, code,stored)
 exec (stack, Fals:code,stored)= (push FF stack, code,stored)
+exec (stack, Neg:code,stored)
+    | top stack == TT = (push FF (pop stack), code,stored)
+    | top stack == FF = (push TT (pop stack), code,stored)
+    | otherwise = error "not bollean on NEG"
 
 exec (stack, State s:code,stored)= (pop stack, code,(s,top stack):stored)
 
+exec (stack, Equ:code,stored)=
+    let v1 = top stack in
+        let v2 = top (pop stack ) in
+            if v1 == v2 then (push TT (pop (pop stack)), code,stored)
+            else (push FF (pop (pop stack)), code,stored)
+
+exec (stack, Le:code,stored)=
+    let N v1 = top stack in
+        let N v2 = top (pop stack ) in
+            if v1 <= v2 then (push TT (pop (pop stack)), code,stored)
+            else (push FF (pop (pop stack)), code,stored)
+                       
+
 exec (stack, Fetch s:code,stored)= 
-        let storedvalue = findStored stored s in
-            (push storedvalue stack, code,(s,top stack):stored)
+        let (newstoredvalues,storedvalue) = findStored stored s in
+            (push storedvalue stack, code,newstoredvalues)
 exec (stack, Add:code,stored)=
     let N v1 = top stack in
         let N v2 = top (pop stack ) in
@@ -62,23 +81,19 @@ exec (stack, Sub:code,stored)=
 
 --state2Str :: State -> String
 --state2Str (_,_,store) = listToString store
-
-stack2Str :: Stack Storeddata -> String
-stack2Str stack 
-    | isEmpty stack = ""
-    | (top stack) == TT  = "True," ++ stack2Str (pop stack)
-    | (top stack) == FF  = "False," ++ stack2Str (pop stack)
-    | otherwise =
-        let N n1 = top stack in
-            (show n1) ++ "," ++ stack2Str (pop stack)
-
 storeddataToString :: Storeddata -> String
 storeddataToString TT = "True"
 storeddataToString FF = "False"
 storeddataToString (N n) = show n
 
+stack2Str :: Stack Storeddata -> String
+stack2Str stack 
+    | isEmpty stack = ""
+    | isEmpty (pop stack) = storeddataToString (top stack)
+    | otherwise = storeddataToString (top stack) ++ "," ++ stack2Str (pop stack)
 state2Str :: [Stored] -> String
 state2Str [] = ""
+state2Str [(s,x)] = s ++ "=" ++ storeddataToString x
 state2Str ((s,x):l) = s ++ "=" ++ storeddataToString x ++ "," ++ state2Str l
 
 runState :: State -> (Stack Storeddata,Code,[Stored])
