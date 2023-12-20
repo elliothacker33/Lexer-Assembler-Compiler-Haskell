5ee92a3721935bf62c901fe845735192d56dcd6a
Tomás Sarmento <up202108778@fe.up.pt>
2023-12-20 16:47:21 +0000

    fixed

--- compiler.hs
+++ compiler.hs
@@ -6,7 +6,7 @@ data Expr = OpAdd Expr Expr
     deriving (Eq, Show)
 
 data Inst =
-    Push Integer | Add | Mult | Sub | Tru | Fals | Equ | Le | And | Neg | Fetch String | State String | Noop |
+    Push Integer | Add | Mult | Sub | Tru | Fals | Equ | Le | And | Neg | Fetch String | Store String | Noop |
     Branch Code Code | Loop Code Code
     deriving Show
 type Code = [Inst]
@@ -60,7 +60,7 @@ exec (And:code,stack,stored)
         andop TT TT = TT
         andop _ _ = FF
 
-exec (State s:code,stack, stored)= (code,pop stack, addStored stored (s,top stack))
+exec (Store s:code,stack, stored)= (code,pop stack, addStored stored (s,top stack))
 
 exec (Branch c1 c2:code,stack, stored)
     | top stack == TT = exec (c1 ++ code,pop stack, stored)
@@ -149,11 +149,11 @@ testAssembler code = (stack2Str stack, state2Str state)
 
 -- tests
 --testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
---testAssembler [Fals,Push 3,Tru,State "var",State "a", State "someVar"] == ("","a=3,someVar=False,var=True")
---testAssembler [Fals,State "var",Fetch "var"] == ("False","var=False")
+--testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
+--testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
 --testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
 --testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
 --testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
 --testAssembler [Push (-20),Push (-21), Le] == ("True","")
---testAssembler [Push 5,State "x",Push 1,Fetch "x",Sub,State "x"] == ("","x=4")
---testAssembler [Push 10,State "i",Push 1,State "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,State "fact",Push 1,Fetch "i",Sub,State "i"]] == ("","fact=3628800,i=1")
+--testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
+--testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store"fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
