9eddaa69c4e50fa273936007dcf00a48a77e9764
Tomás Sarmento <up202108778@fe.up.pt>
2023-12-24 12:50:08 +0000

    fixed some errors

--- lexer.hs
+++ lexer.hs
@@ -1,9 +1,10 @@
 --  This file implements a lexer for haskell
-
+import Data.List
+import Data.Char
 -- All tokens that our lexer recognize as valid Tokens
 data TokenType =
   -- Special Tokens
-  | TOK_SPECIAL
+  TOK_SPECIAL
 
   -- Literals
   | TOK_INT
@@ -19,19 +20,19 @@ data TokenType =
   deriving (Show)
 
 data Position = Position {
-  line :: Int,
-  column :: Int
+  line :: Integer,
+  column :: Integer
 } deriving (Show)
 
 data TokenValue =
   -- Special Token Values
-  | TokenOpenParenthesis
+   TokenOpenParenthesis
   | TokenClosedParenthesis
   | TokenEndOfStatement
 
   -- Literal Values
   | TokenInt Integer
-  | TokenBool Boolean 
+  | TokenBool Bool
 
   -- Identifier and Keyword Values
   | TokenIdent String
@@ -49,7 +50,9 @@ data TokenValue =
   -- Comparison Operators
   | TokenEq
   | TokenNe
-
+  | TokenLe
+  | TokenIntEq
+  |TokenBoolEq
   -- Logical Operators
   | TokenNot
   | TokenAnd
@@ -83,54 +86,54 @@ stringToInt str = foldl (\acc c -> acc * 10 + charToInt c) 0 str
 
 charToInt :: Char -> Integer
 charToInt c 
- | '0' = 0
- | '1' = 1
- | '2' = 2
- | '3' = 3
- | '4' = 4
- | '5' = 5
- | '6' = 6
- | '7' = 7
- | '8' = 8
- | '9' = 9
- | otherwise = error $"Unexpected digit"
+ | c == '0' = 0
+ | c == '1' = 1
+ | c == '2' = 2
+ | c == '3' = 3
+ | c == '4' = 4
+ | c == '5' = 5
+ | c == '6' = 6
+ | c == '7' = 7
+ | c == '8' = 8
+ | c =='9' = 9
+ | otherwise = error "Unexpected digit"
 
 -- Make is valid Identifier 
 lexerOperator :: String -> TokenValue
 lexerOperator op =
   case op of 
-    '+' = TokenPlus
-    '-' = TokenSub
-    '*' = TokenMult
-    "&&" = TokenAnd
-    ":= " = TokenAssign
-    "!=" = TokenLe
-    "==" = TokenIntEq
-    "=" = TokenBoolEq
-    _  = error $ "Lexer Error: Operator {" ++ op ++ "} does not exist"
+    "+" -> TokenPlus
+    "-" -> TokenSub
+    "*" -> TokenMult
+    "&&" -> TokenAnd
+    ":= " -> TokenAssign
+    "!=" -> TokenLe
+    "==" -> TokenIntEq
+    "=" -> TokenBoolEq
+    _  -> error $ "Lexer Error: Operator {" ++ op ++ "} does not exist"
 
 lexerKeyword :: String -> TokenValue
 lexerKeyword keyword =
   case keyword of 
-    "while" = TokenWhile
-    "do" = TokenDo
-    "if" = TokenIf
-    "else" = TokenElse
-    "then" = TokenThen
-    _  = error $ "Lexer Error: Keyword {" ++ keyword ++ "} does not exist"
+    "while" -> TokenWhile
+    "do" -> TokenDo
+    "if" -> TokenIf
+    "else" -> TokenElse
+    "then" -> TokenThen
+    _  -> error $ "Lexer Error: Keyword {" ++ keyword ++ "} does not exist"
 
 lexerSpecial special =
   case special of 
-    ";" = TokenEndOfStatement
-    "(" = TokenOpenParenthesis
-    ")" = TokenClosedParenthesis
-    _ = error $ "Lexer Error: Keyword {" ++ special ++"} does not exist"
+    ";" -> TokenEndOfStatement
+    "(" -> TokenOpenParenthesis
+    ")" -> TokenClosedParenthesis
+    _ -> error $ "Lexer Error: Keyword {" ++ special ++"} does not exist"
 
 lexerBool bool =
-  case special of
-    "True" = TokenBool True
-    "False" = TokenBool False
-    _ = error $ "Lexer Error: Keyword {" ++ bool ++"} does not exist"
+  case bool of
+    "True" -> TokenBool True
+    "False" -> TokenBool False
+    _ -> error $ "Lexer Error: Keyword {" ++ bool ++"} does not exist"
 
 lexer :: String -> [Token]
 lexer = lexer_aux 1 1 
@@ -140,20 +143,20 @@ lexer_aux line column [] = []
 lexer_aux line column (x:xs)
   -- Handle Numbers
   | isDigit x =
-     let (numeric, rest) = span isDigit (x:xs)
-          newPosition = Position line column
-          newType = TOK_INT
-          newValue = TokenInt (stringToInt numericPart)
+    let (numeric, rest) = span isDigit (x:xs)
+        newPosition = Position line column
+        newType = TOK_INT
+        newValue = TokenInt (stringToInt numeric)
       in Token newType newValue newPosition : lexer_aux line (column + fromIntegral (length numeric)) rest
    -- Handle Identifiers and Keywords
    | isAlpha x =
-      let (ident, rest) = span isAlpha (x:xs)
+    let (ident, rest) = span isAlpha (x:xs)
         newPosition = Position line column
-        newType = 
+        newType  
           | ident `elem` validKeywords = TOK_KEYWORD 
           | ident `elem` validBool = TOK_BOOL 
           | otherwise = TOK_IDENT
-        newValue =
+        newValue 
           | ident `elem` validKeywords = lexerKeyword ident
           | ident `elem` validBool = lexerBool ident
           | otherwise = TokenIdent ident
@@ -162,15 +165,16 @@ lexer_aux line column (x:xs)
   -- Handle Operators
   | x `elem` validOperators =
           let (operator, rest) = span validOperators (x:xs)
-          newPosition = Position line column
-          newType = TOK_OPERATOR
-          newValue = lexerOperator operator
+              newPosition = Position line column
+              newType = TOK_OPERATOR
+              newValue = lexerOperator operator
           in Token newType newValue newPosition : lexer_aux line (column + fromIntegral (length operator)) rest
   -- Handle Special chars
   | x `elem` validSpecialChars =
-          newPosition = Position line column
-          newType = TOK_SPECIAL
-          newValue = lexerSpecial x
+        let (operator, rest) = span validOperators (x:xs)
+            newPosition = Position line column
+            newType = TOK_SPECIAL
+            newValue = lexerSpecial x
           in Token newType newValue newPosition : lexer_aux line (column + 1) xs
   | isSpace x = 
       lexer_aux line (column + 1) xs
