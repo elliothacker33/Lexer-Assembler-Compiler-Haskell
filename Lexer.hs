a68dc7d55126ef7ea97e3de9eaf4219cbf32b252
Tomas Pereira <up202108845@edu.fe.up.pt>
2023-12-29 14:54:46 +0000

    Lexer documented

--- Lexer.hs
+++ Lexer.hs
@@ -20,11 +20,13 @@ data TokenType =
 
   deriving (Show)
 
+-- Position of Token in original string
 data Position = Position {
   line :: Int,
   column :: Int
 } deriving (Show)
 
+-- Token value can be different depending on the token.
 data TokenValue =
   -- Special Token Values
   TokenOpenParenthesis
@@ -63,12 +65,14 @@ data TokenValue =
 
   deriving (Show,Eq)
 
+-- Represents a Token with type, value, and position information
 data Token = Token {
   tokenType :: TokenType,
   tokenValue :: TokenValue,
   tokenPosition :: Position
 } 
 
+-- Overloading of show function for a better display of a Token.
 instance Show Token where
   show (Token token_type token_value token_position) =
     " \nToken:\n"
@@ -76,35 +80,43 @@ instance Show Token where
     ++ "  Value: " ++ show token_value ++ "\n"
     ++ "  Position: " ++ show token_position ++ "\n"
 
+-- List of valid operators in this language.
 validOperators :: [String]
 validOperators = ["+", "-", "*", "=", ":","not","and","<"]
 
+-- List of valid keywords in this language.
 validKeywords :: [String]
 validKeywords = ["while","do","if","then","else"]
 
+-- List of valid boolean values in this language.
 validBool :: [String]
 validBool = ["True","False"]
 
+-- List of valid special characters in this language.
 validSpecialChars :: [String]
 validSpecialChars = ["(",")",";"]
 
+-- Helper function to check if an element is present in a list
 my_elem :: String -> [String] -> Bool
 my_elem el [] = False
 my_elem el (x:xs)
   | (x == el) = True
   | otherwise = my_elem el xs
 
+-- Helper function to split a string into two parts based on a function.
 my_span :: (Char -> Bool) -> String -> (String, String)
 my_span _ [] = ([], [])
 my_span f (x:xs)
   | f x = let (matched, rest) = my_span f xs in (x : matched, rest)
   | otherwise = ([], x:xs)
 
+-- Helper function to convert a string to an integer.
 stringToInt :: String -> Int
 stringToInt str = case str of
   ('-' : rest) -> -1 * foldl (\acc c -> acc * 10 + charToInt c) 0 rest
   _ -> foldl (\acc c -> acc * 10 + charToInt c) 0 str
 
+-- Helper function to convert a character in a integer.
 charToInt :: Char -> Int
 charToInt c
   | c == '0' = 0
@@ -119,7 +131,7 @@ charToInt c
   | c == '9' = 9
   | otherwise = error $ "Unexpected digit"
 
--- Make is valid Identifier 
+-- Converts  a operator string to it's corresponding token value.
 lexerOperator :: String -> TokenValue
 lexerOperator op =
   case op of 
@@ -135,6 +147,7 @@ lexerOperator op =
     "=" -> TokenBoolEq
     _  -> error $ "Lexer Error: Operator {" ++ op ++ "} does not exist"
 
+-- Converts a keyword string to it's corresponding token value.
 lexerKeyword :: String -> TokenValue
 lexerKeyword keyword =
   case keyword of 
@@ -145,6 +158,7 @@ lexerKeyword keyword =
     "then" -> TokenThen
     _  -> error $ "Lexer Error: Keyword {" ++ keyword ++ "} does not exist"
 
+-- Converts a special character string to it's corresponding token value.
 lexerSpecial :: String -> TokenValue
 lexerSpecial special =
   case special of 
@@ -153,16 +167,18 @@ lexerSpecial special =
     ")" -> TokenClosedParenthesis
     _ -> error $ "Lexer Error: Keyword {" ++ special ++"} does not exist"
 
+-- Converts a boolean string to it's corresponding token value.
 lexerBool :: String -> TokenValue
 lexerBool bool =
   case bool of
     "True" -> TokenBool True
     "False" -> TokenBool False
     _ -> error $ "Lexer Error: Keyword {" ++ bool ++"} does not exist"
-
+-- Main lexer function that processes the input string and produces a list of tokens
 lexer :: String -> [Token]
 lexer = lexer_aux 1 1 
 
+-- Helper lexer function with additional parameters for line and column tracking
 lexer_aux :: Int -> Int -> String -> [Token]
 lexer_aux line column [] = []
 lexer_aux line column (x:xs)
