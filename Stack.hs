e4230b084efcf0a61db648213b9c8e0fa85a9e0b
Tomas Pereira <up202108845@edu.fe.up.pt>
2024-01-01 17:53:54 +0000

    commented stack

--- Stack.hs
+++ Stack.hs
@@ -1,16 +1,28 @@
 module Stack (Stack, -- exportar o tipo
 push, pop, top, -- e as operações
 createEmptyStack, isEmpty) where
+
+-- Data stack 
 data Stack a = Stk [a] -- implementação usando listas
+
+-- Push elements to the stack
 push :: a -> Stack a -> Stack a
 push x (Stk xs) = Stk (x:xs)
+
+-- Retrieve/Pop elements out of the stack
 pop :: Stack a -> Stack a
 pop (Stk (_:xs)) = Stk xs
 pop _ = error "Stack.pop: empty stack"
+
+-- Top element in the stack
 top :: Stack a -> a
 top (Stk (x:_)) = x
+
+-- Create empty stack (no elements)
 createEmptyStack :: Stack a
 createEmptyStack = Stk []
+
+-- Check if stack has no elements
 isEmpty :: Stack a -> Bool
 isEmpty (Stk [])= True
 isEmpty (Stk _) = False
